1586b90bec316acde01df556ff09a411db274cab
MaciejLitwiniec <litwiniec.maciej@gmail.com>
2017-08-09 20:49:05 +0200

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'person x',
-    text: 'hello!'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'person x',
+  //   text: 'hello!'
+  // });
 
 });
 
--- server/server.js
+++ server/server.js
@@ -14,14 +14,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-      from: 'abc',
-      text: 'def',
-      createdAt: 123
-  });
+  //socket.emit emits event to single connection
+  //io.emit emits event to every connection
+  // socket.emit('newMessage', {
+  //     from: 'abc',
+  //     text: 'def',
+  //     createdAt: 123
+  // });
 
   socket.on('createMessage', (message) => {
     console.log('Created message', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
