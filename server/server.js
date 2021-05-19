a4e4eb6e30de2a84775259fb014e85af3781e240
Mario Pece <pexproduction@gmail.com>
2021-05-18 18:29:36 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,9 +15,43 @@ var users = new Users();
 
 app.use(express.static(publicPath));
 
+
+var players = [];
+
+function Player (id) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.entity = null;
+}
+
+
+
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  
+  
+      socket.on ('initialize', function () {
+            var idNum = players.length;
+            var newPlayer = new Player (idNum);
+            players.push (newPlayer);
+
+            socket.emit ('playerData', {id: idNum, players: players});
+            socket.broadcast.emit ('playerJoined', newPlayer);
+    });
+
+    socket.on ('positionUpdate', function (data) {
+            players[data.id].x = data.x;
+            players[data.id].y = data.y;
+            players[data.id].z = data.z;
+
+        socket.broadcast.emit ('playerMoved', data);
+    });
+  
+  
+  
 
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
